2c1a66e50196849efc2f267e547bedae5c614d1e
Anthony Wilcox <35226681+tonytins@users.noreply.github.com>
2019-11-29 12:55:48 -0500

    Basic lakes
    - Generate basic lakes within the empty spots generated by the terrian
    - Ported over Micropolis' icons

--- src/world.gd
+++ src/world.gd
@@ -2,8 +2,7 @@ extends Node2D
 
 var noise : OpenSimplexNoise
 var map_size = Vector2(80, 60)
-var grass_cap = 0.5
-var road_caps = Vector2(0.3, 0.05)
+var terrian_cap = 0.3
 
 func _ready():
 	randomize()
@@ -13,13 +12,21 @@ func _ready():
 	noise.period = 12
 	
 	make_terrian_map()
-
+	make_water()
 
 func make_terrian_map():
 	for x in map_size.x:
 		for y in map_size.y:
 			var a = noise.get_noise_2d(x, y)
-			if a < grass_cap:
+			if a < terrian_cap:
 				$Terrian.set_cell(x, y, 0)
 
 	$Terrian.update_bitmask_region(Vector2(0.0, 0.0), Vector2(map_size.x, map_size.y))
+	
+func make_water():
+	for x in map_size.x:
+		for y in map_size.y:
+			if $Terrian.get_cell(x, y):
+				$Water.set_cell(x, y, 0)
+				
+	$Water.update_bitmask_region(Vector2(0.0, 0.0), Vector2(map_size.x, map_size.y))
