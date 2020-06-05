7845b1f08ffe3bae49943503272210d804e86a75
Anthony Foxclaw <noreply@tonytins.xyz>
2020-06-05 13:00:13 -0400

    World is now seperate scene

--- src/newgame.gd
+++ src/newgame.gd
@@ -4,4 +4,4 @@ func _on_CreateBtn_pressed():
 	var city_name = $CityNameEdit.text
 	CityData.city_name = city_name
 	
-	get_tree().change_scene("res://src/world.tscn")
+	get_tree().change_scene("res://src/game.tscn")
