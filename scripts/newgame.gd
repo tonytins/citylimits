f5c7355d5a6ca04c7bfea394c860d1766cb86540
Anthony Foxclaw <noreply@tonytins.xyz>
2020-06-11 00:05:09 -0400

    New(ish) icon
    - Reorganized project directories

---
+++ scripts/newgame.gd
@@ -0,0 +1,7 @@
+extends Control
+
+func _on_CreateBtn_pressed():
+	var city_name = $CityNameEdit.text
+	CityData.city_name = city_name
+	
+	get_tree().change_scene("res://scenes/game.tscn")
--- src/newgame.gd
+++
@@ -1,7 +0,0 @@
-extends Control
-
-func _on_CreateBtn_pressed():
-	var city_name = $CityNameEdit.text
-	CityData.city_name = city_name
-	
-	get_tree().change_scene("res://src/game.tscn")
