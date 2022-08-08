40e68bb7e5580c3686c1552fb20654ee476fbec3
Tony Bark <35226681+tonytins@users.noreply.github.com>
2022-08-08 16:12:17 -0400

    Renamed news_outlets_path to full_path

--- scripts/autoload/jsonhelper.gd
+++ scripts/autoload/jsonhelper.gd
@@ -2,9 +2,9 @@ extends Node
 
 func key_value(json_path, json_file, key, is_dictionary = false):
 	var file = File.new()
-	var news_outlets_path = str(json_path + json_file);	
-	if file.file_exists(news_outlets_path):
-		file.open(news_outlets_path, File.READ)
+	var full_path = str(json_path + json_file);	
+	if file.file_exists(full_path):
+		file.open(full_path, File.READ)
 		var result = parse_json(file.get_as_text())
 		if is_dictionary == true:
 			result.clear()
